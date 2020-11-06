a46254dca511a739b020b8bd67eb208923ac4eff
barry <3708366+worksofliam@users.noreply.github.com>
2020-11-06 15:51:07 +0000

    Port to use environment variable le

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const RPG = require('./rpg/RPG');
 const bodyParser = require('body-parser');
 const express = require('express')
 const app = express();
-const port = 9123;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
